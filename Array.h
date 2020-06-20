3af7625ad4eed8f575672463b84839dae77427dd
rond <ron.dahan@campus.technion.ac.il>
2020-06-20 16:19:16 +0300

    f2

--- Array.h
+++ Array.h
@@ -7,94 +7,101 @@
 
 #include <iostream>
 
-class BadAccess : public std::exception {
-public:
-    const char* what() const _NOEXCEPT override
-    {
-        return "Bad Access";
-    }
-};
-
-template <class T>
-class Array{
-    T* data;
-    int length;
-public:
-    explicit Array(int size);
-    Array(const Array<T>& a);
-    ~Array();
-    Array& operator=(const Array<T>& a);
-    int size() const;
-    T& operator[](int index);
-    const T& operator[](int index) const;
-};
 
 
-template<class T>
-Array<T>::Array(int size) {
-    data = new T[size];
-    length = size;
-}
+namespace mtm {
 
-template<class T>
-Array<T>::Array(const Array<T> &a) {
-    data = new T[a.size()];
-    length = a.size();
-    for (int i = 0; i < a.size(); ++i)
-    {
-        data[i] = a[i];
-    }
-}
-
-template<class T>
-Array<T>::~Array() {
-    delete[] data;
-}
-
-template<class T>
-Array<T>& Array<T>::operator=(const Array<T> &a) {
-    if(this == &a)
-    {
-        return this;
-    }
-    T* temp_array = new T[a.size()];
-    delete[] data;
-    for (int i = 0; i < a.size(); ++i)
-    {
-        try {
-            temp_array[i] = a[i];
+    class BadAccess : public std::exception {
+    public:
+        const char* what() const _NOEXCEPT override
+        {
+            return "Bad Access";
         }
-        catch (const std::exception& e){
-            delete[] temp_array;
-            throw e;
+    };
+
+
+    template<class T>
+    class Array {
+        T *data;
+        int length;
+    public:
+        explicit Array(int size);
+
+        Array(const Array<T> &a);
+
+        ~Array();
+
+        Array &operator=(const Array<T> &a);
+
+        int size() const;
+
+        T &operator[](int index);
+
+        const T &operator[](int index) const;
+    };
+
+
+    template<class T>
+    Array<T>::Array(int size) {
+        data = new T[size];
+        length = size;
+    }
+
+    template<class T>
+    Array<T>::Array(const Array<T> &a) {
+        data = new T[a.size()];
+        length = a.size();
+        for (int i = 0; i < a.size(); ++i) {
+            data[i] = a[i];
         }
     }
-    data = temp_array;
-    length = a.size();
-    return *this;
-}
 
-template<class T>
-int Array<T>::size() const {
-    return length;
-}
-
-template<class T>
-T &Array<T>::operator[](int index) {
-    if(index < 0 || index >= this->size())
-    {
-        throw BadAccess();
+    template<class T>
+    Array<T>::~Array() {
+        delete[] data;
     }
-    return data[index];
-}
 
-template<class T>
-const T& Array<T>::operator[](int index) const {
-    if(index < 0 || index >= this->size())
-    {
-        throw BadAccess();
+    template<class T>
+    Array<T> &Array<T>::operator=(const Array<T> &a) {
+        if (this == &a) {
+            return this;
+        }
+        T *temp_array = new T[a.size()];
+        delete[] data;
+        for (int i = 0; i < a.size(); ++i) {
+            try {
+                temp_array[i] = a[i];
+            }
+            catch (const std::exception &e) {
+                delete[] temp_array;
+                throw e;
+            }
+        }
+        data = temp_array;
+        length = a.size();
+        return *this;
     }
-    return data[index];
+
+    template<class T>
+    int Array<T>::size() const {
+        return length;
+    }
+
+    template<class T>
+    T &Array<T>::operator[](int index) {
+        if (index < 0 || index >= this->size()) {
+            throw BadAccess();
+        }
+        return data[index];
+    }
+
+    template<class T>
+    const T &Array<T>::operator[](int index) const {
+        if (index < 0 || index >= this->size()) {
+            throw BadAccess();
+        }
+        return data[index];
+    }
 }
 
 #endif //HW3PARTB_ARRAY_H
--- Matrix.h
+++ Matrix.h
@@ -5,9 +5,99 @@
 #ifndef HW3PARTB_MATRIX_H
 #define HW3PARTB_MATRIX_H
 
-template <class T>
-class Matrix{
+#include "Array.h"
+#include "Auxiliaries.h"
 
-};
+namespace mtm {
+    template<class T>
+    class Matrix {
+        Array<Array<T>> row;
+        Dimensions dim;
+        Dimensions getDimensions() const;
+    public:
+        class iterator;
+        iterator begin();
+        iterator end();
+        class const_iterator;
+        const_iterator begin() const;
+        const_iterator end() const;
+        explicit Matrix(const Dimensions& dimensions = Dimensions(1,1), int value = 0);
+        Matrix(const Matrix& matrix);
+        ~Matrix();
+        Matrix& operator=(const Matrix& matrix);
+        friend Matrix operator+(const Matrix& matrix1, const Matrix& matrix2);
+        Matrix& operator+=(const int value);
+        Matrix operator-() const ;
+        friend Matrix operator-(const Matrix& matrix1, const Matrix& matrix2);
+        int& operator()(int row_num,int col_num);
+        const int& operator()(int row_num,int col_num) const;
+        friend std::ostream& operator<<(std::ostream& os, const Matrix& matrix);
+        Matrix transpose() const ;
+        static Matrix Identity(int dim);
+        int height() const;
+        int width() const;
+        int size() const;
+    };
+
+    template <class T>
+    std::ostream& operator<<(std::ostream& os, const Matrix<T>& matrix);
+
+    template <class T>
+    Matrix<T> operator+(const Matrix<T>& matrix1, const Matrix<T>& matrix2);
+    template <class T>
+    Matrix<T> operator+(const Matrix<T>& matrix, const int value);
+    template <class T>
+    Matrix<T> operator+(const int value, const Matrix<T>& matrix);
+    template <class T>
+    Matrix<T> operator-(const Matrix<T>& matrix1, const Matrix<T>& matrix2);
+    template <class T>
+    Matrix<T> operator<(Matrix<T>& matrix, int num);
+    template <class T>
+    Matrix<T> operator<=(Matrix<T>& matrix, int num);
+    template <class T>
+    Matrix<T> operator>(Matrix<T>& matrix, int num);
+    template <class T>
+    Matrix<T> operator>=(Matrix<T>& matrix, int num);
+    template <class T>
+    Matrix<T> operator==(Matrix<T>& matrix, int num);
+    template <class T>
+    Matrix<T> operator!=(Matrix<T>& matrix, int num);
+    template <class T>
+    bool any(const Matrix<T>& matrix);
+    template <class T>
+    bool all(const Matrix<T>& matrix);
+
+    template <class T>
+    class Matrix<T>::iterator{
+        Matrix* matrix;
+        int index;
+        friend class IntMatrix;
+        iterator(Matrix* matrix, int index);
+    public:
+        int& operator*();
+        iterator& operator++(); // prefix (++it)
+        iterator operator++(int);
+        bool operator==(const iterator& it) const;
+        bool operator!=(const iterator& it) const;
+        iterator(const iterator&) = default;
+        iterator& operator=(const iterator&) = default;
+    };
+
+    template <class T>
+    class Matrix<T>::const_iterator{
+        const Matrix* matrix;
+        int index;
+        friend class IntMatrix;
+        const_iterator(const Matrix* matrix, int index);
+    public:
+        const int& operator*() const;
+        const_iterator& operator++(); // prefix (++it)
+        const_iterator operator++(int);
+        bool operator==(const const_iterator& it) const;
+        bool operator!=(const const_iterator& it) const;
+        const_iterator(const const_iterator&) = default;
+        const_iterator& operator=(const const_iterator&) = default;
+    };
+}
 
 #endif //HW3PARTB_MATRIX_H
